0c5ad08f28d13ee0d03ab23e8798393737bc0eed
Akshat Arora <akshatarora826@gmail.com>
2017-10-31 01:04:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
-var spawn = require("child_process").spawn;
-
+var  fs = require('fs')
+var csv = require('csvtojson')
+var PythonShell = require('python-shell')
 var express = require('express')
+var path = process.cwd()
 var app = express()
+var session = require('express-session')
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser')
 var serv = require('http').Server(app);
+var passport = require('passport')
+var flash = require('connect-flash')
 var ccname
 var cfname
 var dat
-
+var port = process.env.PORT || 8080;
+var morgan = require('morgan')
+var mongoose = require('mongoose')
 //app.set('view engine','ejs')
+var configDB = require('./config/database.js')
+mongoose.connect(configDB.url)
+require(path+'/config/passport')(passport);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(session({secret: 'kgi353927$&^*gjhf7',
+         saveUninitialized: true,
+         resave: true}));
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+
 
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection',function(socket){
 	console.log('socket connection')
+
 
 
 socket.on('ccvalue',function(data){
@@ -21,24 +45,46 @@ socket.on('ccvalue',function(data){
        
       console.log(ccname)
       console.log(cfname)
-
-     var process = spawn('python',["./hello.py",ccname,cfname]); 
-process.stdout.on('data', function (data){
-// Do something with the data returned from python script
-dat=data.toString('utf8')
-console.log(data.toString('utf8'));
+    if(ccname)
+{
+var options = {
+    mode: 'text',
+    args: [ccname]
+};
+PythonShell.run('ccscrapper.py', options, function (err, results) {
+    if (err) throw err;
+    // results is an array consisting of messages collected during execution
+    console.log('results: %j', results);
+    
 });
+
+
+}
+    if(cfname)
+{
+var options = {
+    mode: 'text',
+    args: [cfname]
+};
+PythonShell.run('cfscrapper.py', options, function (err, results) {
+    if (err) throw err;
+    // results is an array consisting of messages collected during execution
+    console.log('results: %j', results);
+    
+});
+
+
+}
+
 
          
     });
 
 });
 
+app.set('view engine','ejs');
 
+require('./app/routes.js')(app,passport);
 
-app.get('/',function(req,res){
-res.render('index.ejs')
-})
-
-serv.listen(8080)
+serv.listen(port);
 console.log("Server Started.")
